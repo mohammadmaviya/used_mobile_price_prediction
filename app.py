f2d421f2e0dc9f32b49a563dd621c9cc3a6167bb
Mohammad Maviya <maviyaansari131@gmail.com>
2025-11-20 18:34:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+with open("used_phone.pkl","rb") as f:
+    model = pickle.load(f)
+
+df = pd.read_csv("used_phone.csv")
+
+brand_model_map = df.groupby('brand')['model'].unique().to_dict()
+brand_list = list(brand_model_map.keys())
+
+st.title("Used Mobile Price Predictor")
+st.header("Enter Phone Details:")
+
+selected_brand = st.selectbox("Select Brand",brand_list)
+selected_model = st.selectbox("Select Model",brand_model_map[selected_brand])
+ram = st.selectbox("RAM(GB)",sorted(df['ram_gb'].unique()))
+storage = st.selectbox("Storage(GB)",sorted(df['storage_gb'].unique()))
+selected_condition = st.selectbox("Condition",df["condition"].unique())
+battery = st.slider("Battery Health (%)",50,100,80)
+age = st.slider("Age of Phone",0,5,1)
+original_price = st.number_input("Original Price (INR)",3000,100000,15000)
+
+from sklearn.preprocessing import LabelEncoder
+
+le_brand = LabelEncoder()
+le_model = LabelEncoder()
+le_condition = LabelEncoder()
+
+df['brand'] = le_brand.fit_transform(df['brand'])
+df['model'] = le_model.fit_transform(df['model'])
+df['condition'] = le_condition.fit_transform(df['condition'])
+
+brand_encoded = le_brand.transform([selected_brand])[0]
+model_encoded = le_model.transform([selected_model])[0]
+condition_encoded = le_condition.transform([selected_condition])[0]
+
+input_data = pd.DataFrame({
+    "brand":[brand_encoded],
+    "model":[model_encoded],
+    "ram_gb":[ram],
+    "storage_gb":[storage],
+    "condition":[condition_encoded],
+    "battery_health":[battery],
+    "age_years":[age],
+    "original_price":[original_price]
+})
+
+if st.button("Predict"):
+    predicted_price = model.predict(input_data)[0]
+    st.success(f"Estimated Used Phone Price :{int(predicted_price): ,}")
